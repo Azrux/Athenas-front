5c8d23a5f3eba64147b00ddcba0d7bfcac928865
Azrux <lausofimarce@gmail.com>
2023-08-12 16:35:38 -0300

    [fix] - Delete unnecessary comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 import { nextui } from '@nextui-org/react'
-// const { nextui } = require('@nextui-org/react');
 
 export default {
   content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
