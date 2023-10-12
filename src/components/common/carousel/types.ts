74e8b53b98f0636634d6afeca8b606c813b0fe35
Azrux <lausofimarce@gmail.com>
2023-10-12 20:56:10 -0300

    [feat] - Create Carousel common component

---
+++ src/components/common/carousel/types.ts
@@ -0,0 +1,7 @@
+import { CarouselProps as DaisyCarouselProps } from 'react-daisyui';
+
+export interface CarouselProps extends Partial<DaisyCarouselProps> {
+	images: string[];
+	classNameCarousel?: string;
+	classNameContainer?: string;
+}
