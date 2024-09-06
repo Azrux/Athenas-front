8c19c35e8e2bb488a9b63bcbfbd37a6790baa757
azrux <lausofimarce@gmail.com>
2024-09-06 00:49:29 -0300

    feat: added landing chore: install leaflet library to add geolocalization

---
+++ src/components/map/map.tsx
@@ -0,0 +1,21 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+const Map = () => {
+	return (
+		<MapContainer
+			center={[-34.658138, -58.761655]}
+			zoom={13}
+			scrollWheelZoom={false}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={[-34.658138, -58.761655]}>
+				<Popup>Athenas Club</Popup>
+			</Marker>
+		</MapContainer>
+	);
+};
+
+export default Map;
