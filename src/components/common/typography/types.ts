31da3805de4d660708b99fd7b8df1f3ded0dcdc1
wellnahuel <nahuelcittadino@gmail.com>
2024-09-09 14:13:31 -0300

    Delete usefull files

--- src/components/common/typography/types.ts
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from "react";
-
-export interface TypographyProps {
-  type?: "title" | "subtitle" | "paragraph" | "caption" | "quote" | "error";
-  children: ReactNode;
-  className?: string;
-}
